ee7a84217c88a85a275b064b4d7d9e5a3f08ea6d
Oleg Shvydkiy <oleg.mason8686@gmail.com>
2022-07-06 03:36:07 +0300

    create

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const bot = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS" ]});
+let config = require("./config.json");
+const { off } = require("process");
+bot.login(config.token);
+
+const prefix = config.commandPrefix;
+
+bot.commands = new Discord.Collection();
+const commands = fs.readdirSync("./commands").filter( file =>file.endsWith(".js"));
+
+const events = fs.readdirSync("./events").filter( file =>file.endsWith(".js"));
+
+
+commands.forEach( file => {
+    const commandName = file.substr(0, file.indexOf("."));
+    const command = require(`./commands/${commandName}`);
+    bot.commands.set(commandName, command);
+});
+
+events.forEach( file=>{
+    const eventName = file.substr(0, file.indexOf("."));
+    const event = require(`./events/${eventName}`);
+    bot.on(eventName, (...args) => event.run(...args));
+})
+    
+
+bot.on("messageCreate", function(message) {
+    if(message.content.startsWith(prefix)){
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const commandName = args.shift();
+        const command = bot.commands.get(commandName);
+        if(!command) return
+
+        command.run(bot, message, args);
+    }   
+});
+
+
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "guildMemberAdd",
+    run: async (member)=>{
+        
+    }
+}
