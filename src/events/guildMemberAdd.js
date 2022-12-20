f487bfa3ce071854a837d8caecb71603a42d6c7f
Oleg Shvydkiy <oleg.mason8686@gmail.com>
2022-12-20 19:47:27 +0200

    fix

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -5,16 +5,22 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     name: "guildMemberAdd",
     run: async (bot, member) => {
-        const newMember = new EmbedBuilder()
-            .setColor("#7FFF00")
-            .setTitle('✅Новый пользователь! ')
-            .setDescription(`${member.user.tag}(${member.user}) только что присоеденился к серверу!`)
-            .setThumbnail(member.user.avatarURL())
-            .setFooter('Всего пользователей: ' + member.guild.memberCount);
-        //отправляем пользователю ссылку на опрос
-        //member.send("https://docs.google.com/forms/d/1Nl95IdjRAmtvg5Aj_Zc7Gz4mCphVd5yYRmO9C3idd_g");
-        // config.welcomeMessageChannel
-        member.guild.channels.cache.get(config.welcomeMessageChannel).send({ embeds: [newMember] });
+        Log.init(bot);
+        try {
+            const newMember = new EmbedBuilder()
+                .setColor("#7FFF00")
+                .setTitle('✅Новый пользователь! ')
+                .setDescription(`${member.user.tag}(${member.user}) только что присоеденился к серверу!`)
+                .setThumbnail(member.user.avatarURL())
+                .setFooter({ text: 'Всего пользователей: ' + member.guild.memberCount });
+            //отправляем пользователю ссылку на опрос
+            //member.send("https://docs.google.com/forms/d/1Nl95IdjRAmtvg5Aj_Zc7Gz4mCphVd5yYRmO9C3idd_g");
+            // config.welcomeMessageChannel
+            member.guild.channels.cache.get(config.welcomeMessageChannel).send({ embeds: [newMember] });
+        } catch (e) {
+            Log.error(`[COMMANDS/memberAdd] Ошибка создания уведомления: ${e}`);
+        }
+
 
 
 
--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,16 +1,23 @@
 const Discord = require("discord.js");
 const config = require("../../config.json");
+const { EmbedBuilder } = require('discord.js');
+
 
 module.exports = {
     name: "guildMemberRemove",
     run: async (bot, member) => {
-        const message = new Discord.MessageEmbed()
-            .setColor("#7FFF00")
-            .setTitle('Пользовтель покинул сервер')
-            .setDescription(`${member.user.tag} только что покинул сервер!`)
-            .setThumbnail(member.user.avatarURL())
-            .setFooter('Всего пользователей: ' + member.guild.memberCount);
-        // config.welcomeMessageChannel
-        member.guild.channels.cache.get(config.welcomeMessageChannel).send({ embeds: [message] });
+        Log.init(bot)
+        try {
+            const message = new EmbedBuilder()
+                .setColor("#7FFF00")
+                .setTitle('Пользовтель покинул сервер')
+                .setDescription(`${member.user.tag} только что покинул сервер!`)
+                .setThumbnail(member.user.avatarURL())
+                .setFooter({ text: 'Всего пользователей: ' + member.guild.memberCount });
+            member.guild.channels.cache.get(config.welcomeMessageChannel).send({ embeds: [message] });
+        } catch (e) {
+            Log.error(`[COMMANDS/memberRemove] Ошибка создания уведомления: ${e}`);
+        }
+
     }
 }
